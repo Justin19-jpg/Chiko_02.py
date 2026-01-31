3b30ff8e45d554547808ea7510f3fcd6ae1859c3
Justin19-jpg <justinsolina19@gmail.com>
2026-02-01 06:19:41 +0800

    Refactor number input and average calculation

--- SOLINA_HO1.py
+++ SOLINA_HO1.py
@@ -1,17 +1,17 @@
-num_list= []
-
 word = input("Enter a word:")
 
-for n in range(len(word)):
-    num = int(input(f"Enter number {n+1}:"))
-    num_list.append(num)
-    
+numbers = []
+
+for n in range(1, 7):
+    num = int(input(f"Enter number {n}:"))
+    numbers.append(num)
+
 print(numbers)
 
 word_length = len(word)
 print("The length of the word is", word_length)
 
-avg = sum(numbers) / len(numbers)
+average = sum(numbers) / len(numbers)
 print("The average of the numbers is", average)
 
 if word_length > average:
@@ -20,5 +20,6 @@ elif word_length < average:
     print(f"The length of the word '{word}' is less than the average.")
 else:
     print(f"The length of the word '{word}' is equal to the average.")
-print(" Thank you ")
+print("thank you")
+    
 
