cb16763b00957fec5c4bace5b90f5f4c7fea44fc
Justin19-jpg <justinsolina19@gmail.com>
2026-02-11 07:15:08 +0800

    Create Simple Profile App using Tkinter

---
+++ Solin_HO2.py
@@ -0,0 +1,19 @@
+import tkinter as Chiko
+
+window = Chiko.Tk()
+window.title("Simple Profile App")
+window.geometry("600x600")
+window.resizable(False, True)
+
+frame = Chiko.Frame(root, padx=20, pady=20)
+frame.pack(anchor="w")
+
+Chiko.Label(frame, text="PROFILE INFORMATION", font=("Arial", 16, "bold")).pack(anchor="w", pady=(0, 15))
+
+Chiko.Label(frame, text="Full Name: Justin Solina", font=("Arial", 12)).pack(anchor="w", pady=5)
+Chiko.Label(frame, text="Age: 19", font=("Arial", 12)).pack(anchor="w", pady=5)
+Chiko.Label(frame, text="Course & Section: BSIT - 1C", font=("Arial", 12)).pack(anchor="w", pady=5)
+Chiko.Label(frame, text="Birthday: October 5, 2006", font=("Arial", 12)).pack(anchor="w", pady=5)
+Chiko.Label(frame, text='Personal Motto: "Dont do it by yourself, Do it with God ."',font=("Arial", 12),  wraplength=550, justify="left").pack(anchor="w", pady=5)
+
+window.mainloop()
