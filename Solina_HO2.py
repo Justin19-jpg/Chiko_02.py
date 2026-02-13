d07d0d4fc2baf0be4a5e100993dd5bac34e72e62
Justin19-jpg <justinsolina19@gmail.com>
2026-02-13 12:32:48 +0800

    Update and rename Solin_HO2.py to Solina_HO2.py

--- Solina_HO2.py
+++ Solina_HO2.py
@@ -5,11 +5,10 @@ window.title("Simple Profile App")
 window.geometry("600x600")
 window.resizable(False, True)
 
-frame = Chiko.Frame(root, padx=20, pady=20)
+frame = Chiko.Frame(Chiko, padx=20, pady=20)
 frame.pack(anchor="w")
 
 Chiko.Label(frame, text="PROFILE INFORMATION", font=("Arial", 16, "bold")).pack(anchor="w", pady=(0, 15))
-
 Chiko.Label(frame, text="Full Name: Justin Solina", font=("Arial", 12)).pack(anchor="w", pady=5)
 Chiko.Label(frame, text="Age: 19", font=("Arial", 12)).pack(anchor="w", pady=5)
 Chiko.Label(frame, text="Course & Section: BSIT - 1C", font=("Arial", 12)).pack(anchor="w", pady=5)
