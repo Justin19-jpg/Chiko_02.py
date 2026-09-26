17aba8a04a685e4b04ae7313ea8744de343ff6e7
Justin19-jpg <justinsolina19@gmail.com>
2026-09-26 16:36:02 +0800

    Create Dukudkduu.py

---
+++ Dukudkduu.py
@@ -0,0 +1,2 @@
+#generate a tablet, mobile version of this design, for mobile 375 px width and 812 height and for tablet is
+#768 px width same 1024 height, include the pop modals, also make the tablet and mobile device responsive. dont change the existing designs
